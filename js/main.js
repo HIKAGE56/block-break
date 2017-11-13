84a1668a0b2be35d7b970a98f9e8f1a96a5b94d9
meko <accelstop@gmail.com>
2017-11-13 23:07:17 +0900

    player and ball
    プレイヤーとボールのクラス作りました。
    ステージの大きさも設定しました。

--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
-<!doctype html>
-<html><head><script src="enchant.js"></script></head>
-<body style="margin:0; padding: 0;">
-<script>
-    enchant(); // initialize
-    var game = new Core(320, 320); // game stage
-    game.preload('img/IMG_1212.PNG'); // preload image
+
+enchant(); // initialize
+window.onload = function(){
+    main();
+};
+
+var SIZE = 32;
+var WIDTH = SIZE * 11;
+var HEIGHT = SIZE * 20;
+function main(){
+    game = new Core(WIDTH, HEIGHT); // game stage
+    game.preload('img/IMG_1212.PNG'); 
     game.fps = 20;
-
+    game.rootScene.backgroundColor = "black";
+    player = new Player(4,17);
+    ball = new Ball(5,16);
     game.onload = function(){
-        var bear = new Sprite(32, 32);
-        bear.image = game.assets['chara1.png'];
-        game.rootScene.addChild(bear);
-        bear.frame = [6, 6, 7, 7];   // select sprite frame
-        
-        bear.tl.moveBy(288, 0, 90)   // move right
-            .scaleTo(-1, 1, 10)      // turn left
-            .moveBy(-288, 0, 90)     // move left
-            .scaleTo(1, 1, 10)       // turn right
-            .loop();                 // loop it
+        game.rootScene.addChild(player);
+        game.rootScene.addChild(ball);
     };
-
     game.start(); // start your game!
-</script>
-</body>
-</html>
+};
+
+
+Player = Class.create(Sprite,{
+    initialize: function(x,y){
+        Sprite.call(this,SIZE*3,SIZE);
+        this.x = x * SIZE;
+        this.y = y * SIZE;
+        this.backgroundColor = "green";
+    },
+    onenterframe: function(){
+        if(this.x<=WIDTH-SIZE*2)if(game.input.right){this.x+=SIZE;}
+        if(this.x>0-SIZE*2)if(game.input.left){this.x-=SIZE;}
+        //if(this.y<=HEIGHT-SIZE*2)if(game.input.down){this.y+=SIZE;}
+        //if(this.y>0)if(game.input.up){this.y-=SIZE;}
+    }
+});
+
+Ball = Class.create(Sprite,{
+    initialize: function(x,y){
+        Sprite.call(this,SIZE,SIZE);
+        this.x = x * SIZE;
+        this.y = y * SIZE;
+        this.backgroundColor = "red";
+        this.Xdir = 0;
+        this.Ydir = 0;
+    },
+    onenterframe: function(){
+        if(this.intersect(player)){
+        }else{
+            this.backgroundColor = "blue";
+        }
+    }
+});
